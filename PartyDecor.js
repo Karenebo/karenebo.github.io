71f0cadb187528461e4f6b537f14f2a99295924c
Karenebo <109280782+Karenebo@users.noreply.github.com>
2022-09-08 07:46:36 -0400

    Add files via upload

--- PartyDecor.js
+++ PartyDecor.js
@@ -5,7 +5,7 @@ let mybutton = document.getElementById("myBtn");
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+  if (document.body.scrollTop > 95 || document.documentElement.scrollTop > 95) {
     mybutton.style.display = "block";
   } else {
     mybutton.style.display = "none";
