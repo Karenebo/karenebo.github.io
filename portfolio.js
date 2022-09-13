6b38f5e021e6c6087fa717fe2f6828e645cd34fd
Karenebo <109280782+Karenebo@users.noreply.github.com>
2022-09-13 13:20:49 -0400

    Add files via upload

--- portfolio.js
+++ portfolio.js
@@ -1,3 +1,20 @@
 
 
 /* come back to this: document.getElementsByTagName("Butterfly icon")[0].style.fontSize = "6vw"; */
+
+let mybutton = document.getElementById("Btn");
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 25 || document.documentElement.scrollTop > 25) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+function topFunction() {
+  document.body.scrollTop = 0; 
+  document.documentElement.scrollTop = 0; 
+}
